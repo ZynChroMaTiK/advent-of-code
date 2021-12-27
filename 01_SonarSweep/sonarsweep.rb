240598631bce6ca2fae58546a6a408a32959d9f4
Brian Darmi <brian.darmi@gmail.com>
2021-12-27 15:09:37 +0800

    part 2 done, comments on both parts

--- 01_SonarSweep/sonarsweep.rb
+++ 01_SonarSweep/sonarsweep.rb
@@ -4,9 +4,20 @@ puts "count #{INPUT.count}"
 puts "first #{INPUT.first}"
 puts "last  #{INPUT.last}"
 
-larger = 0
-for i in 0..(INPUT.length - 2) do
+larger1 = 0
+larger2 = 0
 
-  larger += 1 if INPUT[i] < INPUT[i+1]
+# Part One
+# Compare between each measurement and the next one
+for i in 0..(INPUT.length - 2) do
+  larger1 += 1 if INPUT[i] < INPUT[i+1]
 end
-puts "larger #{larger}"
+puts "larger1 #{larger1}"
+
+# Part Two
+# No need to sum common elements between a window and the next one
+# Compare between first of A and last of B, and so on
+for i in 0..(INPUT.length - 4) do
+  larger2 += 1 if INPUT[i] < INPUT[i+3]
+end
+puts "larger2 #{larger2}"
