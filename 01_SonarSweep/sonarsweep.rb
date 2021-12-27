9c699fd2d04fa2bef68c5cf70f00938d0b928a54
Brian Darmi <brian.darmi@gmail.com>
2021-12-27 14:56:16 +0800

    parse lines to integer

--- 01_SonarSweep/sonarsweep.rb
+++ 01_SonarSweep/sonarsweep.rb
@@ -1,7 +1,12 @@
-INPUT = File.readlines('input.txt')
+INPUT = File.readlines('input.txt').map(&:to_i)
+
+puts "count #{INPUT.count}"
+puts "first #{INPUT.first}"
+puts "last  #{INPUT.last}"
 
 larger = 0
 for i in 0..(INPUT.length - 2) do
+
   larger += 1 if INPUT[i] < INPUT[i+1]
 end
-puts larger # 1386
+puts "larger #{larger}"
