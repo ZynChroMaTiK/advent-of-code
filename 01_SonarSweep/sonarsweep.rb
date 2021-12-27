6cda7e1ad3cf03ab90a2db78f2464e6ac15668a0
Brian Darmi <brian.darmi@gmail.com>
2021-12-27 16:21:21 +0800

    day 2 complete

--- 01_SonarSweep/sonarsweep.rb
+++ 01_SonarSweep/sonarsweep.rb
@@ -2,20 +2,15 @@
 
 INPUT = File.readlines("#{__dir__}/input.txt").map(&:to_i)
 
-puts "count #{INPUT.count}"
-puts "first #{INPUT.first}"
-puts "last  #{INPUT.last}"
-
-larger1 = 0
-larger2 = 0
-
 # Part One
 # Compare between each measurement and the next one
+larger1 = 0
 (0..(INPUT.length - 2)).each { |i| larger1 += 1 if INPUT[i] < INPUT[i + 1] }
 puts "larger1 #{larger1}"
 
 # Part Two
 # No need to sum common elements between a window and the next one
 # Compare between first of A and last of B, and so on
+larger2 = 0
 (0..(INPUT.length - 4)).each { |i| larger2 += 1 if INPUT[i] < INPUT[i + 3] }
 puts "larger2 #{larger2}"
--- 02_Dive/dive.rb
+++ 02_Dive/dive.rb
@@ -1,6 +1,29 @@
+# frozen_string_literal: true
+
 INPUT = File.readlines("#{__dir__}/input.txt").map do |i|
   cmd = i.split
-  {dir: cmd[0], stp: cmd[1].to_i}
+  [cmd[0], cmd[1].to_i]
 end
 
-puts INPUT.first
+# Part One
+x = y = 0
+INPUT.each do |i|
+  x += i[1] if i[0] == 'forward'
+  y += i[1] if i[0] == 'down'
+  y -= i[1] if i[0] == 'up'
+end
+puts "x #{x}, y #{y}" # 2162, 1051
+puts "x*y = #{x * y}" # 2272262
+
+#Part Two
+a = x = y = 0
+INPUT.each do |i|
+  if i[0] == 'forward'
+    x += i[1]
+    y += i[1] * a
+  end
+  a += i[1] if i[0] == 'down'
+  a -= i[1] if i[0] == 'up'
+end
+puts "x #{x}, y #{y}" #
+puts "x*y = #{x * y}" #
