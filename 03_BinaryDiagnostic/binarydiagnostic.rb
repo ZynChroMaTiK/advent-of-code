c51cd85c0e79666116afcd08896e6e033260854e
Brian Darmi <brian.darmi@gmail.com>
2021-12-28 18:45:34 +0800

    code even more refactored

--- 03_BinaryDiagnostic/binarydiagnostic.rb
+++ 03_BinaryDiagnostic/binarydiagnostic.rb
@@ -3,16 +3,16 @@
 # Convert input to arrays of integers
 INPUT = File.readlines("#{__dir__}/input.txt", chomp: true).map { |n| n.each_char.map(&:to_i) }
 # Compare sums on each bit index to get average bits as gamma
-sum = INPUT.transpose.map { |n| n.sum / (INPUT.count / 2.0).ceil }
+sum = INPUT.transpose.map { |n| n.sum / (INPUT.count * 0.5).ceil }
 # Gamma bits to decimal
 gam = sum.join.to_i(2)
 # Epsilon is one's complement of gamma
 eps = gam ^ 0xfff # Flip only 12 bits
 
-puts "gamma   #{gam}"
-puts "epsilon #{eps}"
+puts "gamma   #{gam}" # 3516
+puts "epsilon #{eps}" # 579
 # Power consumption is gamma * epsilon
-puts "power   #{gam * eps}"
+puts "power   #{gam * eps}" # 2035764
 
 
 # Part Two
@@ -25,13 +25,13 @@ co2 = INPUT - oxy
   # Oxygen generator
   if oxy.count > 1
     # Select most common or 1 if equal
-    avg = oxy.map { |n| n[i] }.sum / (oxy.count / 2.0).ceil
+    avg = oxy.map { |n| n[i] }.sum / (oxy.count * 0.5).ceil
     oxy = oxy.select { |n| n[i] == avg }
   end
   # CO2 scrubber
   if co2.count > 1
     # Select least common or 0 if equal
-    avg = co2.map { |n| n[i] }.sum / (co2.count / 2.0).ceil
+    avg = co2.map { |n| n[i] }.sum / (co2.count * 0.5).ceil
     co2 = co2.select { |n| n[i] == avg ^ 1 }
   end
 end
@@ -39,7 +39,7 @@ end
 oxy = oxy[0].join.to_i(2)
 co2 = co2[0].join.to_i(2)
 
-puts "oxygen  #{oxy}"
-puts "co2     #{co2}"
+puts "oxygen  #{oxy}" # 3311
+puts "co2     #{co2}" # 851
 # Life support is oxygen * co2
-puts "life    #{oxy * co2}"
+puts "life    #{oxy * co2}" # 2817661
