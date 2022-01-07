21b00f57f77ed0e01059f0822ac52c93804f9f01
Brian Darmi <brian.darmi@gmail.com>
2022-01-07 17:20:39 +0800

    Part Two started

--- 06_Lanternfish/lanternfish.rb
+++ 06_Lanternfish/lanternfish.rb
@@ -1,12 +1,21 @@
-days = 80
+# frozen_string_literal: true
+
 fishes = File.read("#{__dir__}/input.txt").strip.split(',').map(&:to_i)
 
 # Part One
 
-days.times do
-  pregs = fishes.count(0)
-  fishes.map! { |f| f == 0 ? 6 : f - 1 }
-  fishes += [8] * pregs
+def p1_spawn(fishes, days)
+  fishes = fishes.dup
+  days.times do
+    pregs = fishes.count(0)
+    fishes.map! { |f| f.zero? ? 6 : f - 1 }
+    fishes += [8] * pregs
+  end
+  fishes.count
 end
 
-p fishes.count
+p p1_spawn(fishes, 80)
+
+# Part Two
+
+# Define a math function instead of a loop to get same results
